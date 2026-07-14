8fe6c6f50458063d107ff8fd677ab89419eedb7a
Nectartist <feli.gajardom@duocuc.cl>
2026-07-14 09:13:59 -0400

    Se crearon los dos principales diccionarios del sistema

---
+++ main.py
@@ -0,0 +1,12 @@
+
+planes = {
+    "F001":["Plan basico", "Mensual", 1, False, False, "Libre"],
+    "F002":["Plan Full", "Mensual", 1, True, True, "Libre"],
+    "F003":["Plan de Estudiante", "Trimestral", 3, False, True, "Tarde"]
+}
+
+inscripciones = {
+    "F001": [14990, 30],
+    "F002": [18990, 10],
+    "F003": [22990, 20]
+}
